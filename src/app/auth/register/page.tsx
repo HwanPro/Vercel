42ff511426eee447af23145afa0e3c05bc876725
HwanPro <168.1.2.0.20a@gmail.com>
2024-12-06 07:07:49 -0500

    Initial commit

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,3 +1,88 @@
+// /app/api/auth/register/route.ts
+
+import { NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
+import prisma from '@/libs/prisma';
+import { sendVerificationEmail } from '@/libs/mail';
+import { generateVerificationToken } from '@/utils/auth';
+
+export async function POST(req: Request) {
+  try {
+    const { username, email, password, lastname } = await req.json();
+
+    if (!username || !email || !password || !lastname) {
+      return NextResponse.json(
+        { message: 'Todos los campos son obligatorios' },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: 'El correo ya está registrado' },
+        { status: 400 }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await prisma.user.create({
+      data: {
+        name: username,
+        email,
+        password: hashedPassword,
+        role: 'client',
+      },
+    });
+
+    // Crear el ClientProfile asociado
+    await prisma.clientProfile.create({
+      data: {
+        profile_first_name: username,
+        profile_last_name: lastname,
+        profile_plan: 'Básico',
+        profile_start_date: new Date(),
+        profile_end_date: new Date(), // Ajusta la fecha según tu lógica
+        profile_phone: '',
+        profile_emergency_phone: '',
+        user_id: user.id,
+      },
+    });
+
+    // Generar y guardar el token de verificación
+    const token = generateVerificationToken();
+    await prisma.verificationToken.create({
+      data: {
+        identifier: email,
+        token,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // Token válido por 24 horas
+      },
+    });
+
+    // Enviar el correo de verificación
+    await sendVerificationEmail(email, token);
+
+    return NextResponse.json(
+      {
+        message:
+          'Usuario registrado con éxito. Revisa tu correo para verificar la cuenta.',
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error('Error al registrar el usuario:', error);
+    return NextResponse.json(
+      { message: 'Error al registrar el usuario' },
+      { status: 500 }
+    );
+  }
+}
+
+
 // /app/auth/register/page.tsx
 
 'use client';
@@ -7,6 +92,7 @@ import { useState } from 'react';
 
 type RegisterData = {
   username: string;
+  lastname: string;
   email: string;
   password: string;
   confirmPassword: string;
@@ -31,6 +117,7 @@ function RegisterPage() {
         method: 'POST',
         body: JSON.stringify({
           username: data.username,
+          lastname: data.lastname,
           email: data.email,
           password: data.password,
         }),
@@ -69,6 +156,19 @@ function RegisterPage() {
           placeholder="Nombre de usuario"
         />
         {errors.username && <span className="text-red-500 text-xs">{errors.username.message}</span>}
+
+        <label htmlFor="lastname" className="text-slate-500 mb-2 block text-sm">
+          Apellido:
+        </label>
+        <input
+          type="text"
+          {...register('lastname', {
+            required: { value: true, message: 'El apellido es obligatorio' },
+          })}
+          className="border p-2 w-full mb-4 text-gray-800"
+          placeholder="Apellido"
+        />
+        {errors.lastname && <span className="text-red-500 text-xs">{errors.lastname.message}</span>}
 
         <label htmlFor="email" className="text-slate-500 mb-2 block text-sm">
           Correo electrónico:
