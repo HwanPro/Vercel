5a13b25770243faa510603e7f66c3e451e72b4a2
HwanPro <168.1.2.0.20a@gmail.com>
2024-12-06 22:43:38 -0500

    db

--- libs/prisma.ts
+++ libs/prisma.ts
@@ -2,6 +2,15 @@
 
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+declare global {
+  // Esta variable solo estará disponible en el modo desarrollo.
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma =
+  globalThis.prisma ||
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
 export default prisma;
