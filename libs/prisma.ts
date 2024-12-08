adc5c26bf754c10e141e2893e3370bbcff0167dc
HwanPro <168.1.2.0.20a@gmail.com>
2024-12-07 19:31:33 -0500

    Prueba

--- libs/prisma.ts
+++ libs/prisma.ts
@@ -1,16 +1,14 @@
-// src/libs/prisma.ts
-
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // Esta variable solo estará disponible en el modo desarrollo.
+  // Esto evita conflictos al trabajar en desarrollo
   var prisma: PrismaClient | undefined;
 }
 
-const prisma =
-  globalThis.prisma ||
-  new PrismaClient();
+const prisma = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
 
 export default prisma;
