41d9c8bbfc6c6ac31170b5bc587cbcf21bb12c54
HwanPro <168.1.2.0.20a@gmail.com>
2025-03-05 19:27:20 -0500

    Actualizacion en clints/dashboard

--- src/app/client/dashboard/page.tsx
+++ src/app/client/dashboard/page.tsx
@@ -66,9 +66,7 @@ export default function ClientDashboard() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch("/api/clients/me", {
-          credentials: "include",
-        });
+        const res = await fetch("/api/clients/me", {});
         if (!res.ok) throw new Error("Error al obtener datos del cliente");
         const data = await res.json();
         setClientData(data);
