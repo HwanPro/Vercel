690f8c34250ff1d84c28917f2e340b286c11ccdb
HwanPro <168.1.2.0.20a@gmail.com>
2025-09-07 16:19:48 -0500

    Change next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ← salta el lint en build
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wolf-gym.s3.us-east-1.amazonaws.com",
+        pathname: "/**", // o "/uploads/**" si prefieres restringir
+      },
+    ],
   },
 };
 
